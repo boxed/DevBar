01a71225712fcbe5410264f0d78d1d0d17310deb
Anders Hovmöller <anders.hovmoller@trioptima.com>
2018-10-17 13:33:48 +0200

    Initial Commit

---
+++ DevBar/AppDelegate.swift
@@ -0,0 +1,166 @@
+//
+//  AppDelegate.swift
+//  DevBar
+//
+//  Created by Anders Hovmöller on 2018-10-17.
+//  Copyright © 2018 Anders Hovmöller. All rights reserved.
+//
+
+import Cocoa
+
+struct Display : Decodable {
+    let symbol : String
+    let title : String
+}
+
+struct Metadata : Decodable {
+    let display: [String: Display]
+}
+
+struct Result : Decodable {
+    let metadata: Metadata
+    let data: [String: [PR]]
+}
+
+struct PR : Decodable {
+    let title: String
+    let url: String
+}
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var preferencesWindow: NSWindow!
+    
+    var statusBarItem : NSStatusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    var timer : Timer? = nil
+    var menu: NSMenu = NSMenu()
+    var menuOpen = false
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        statusBarItem.button?.title = ""
+        statusBarItem.menu = menu
+        menu.delegate = self
+        self.update()
+
+        // update every second
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.update), userInfo: nil, repeats: true)
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    func menuWillOpen(_ menu: NSMenu) {
+        menuOpen = true
+    }
+    
+    func menuDidClose(_ menu: NSMenu) {
+        menuOpen = false
+    }
+    
+    func createMenuItems(_ prs: [PR], title: String) {
+        guard !prs.isEmpty else { return }
+        
+        menu.addItem(NSMenuItem(title: title, action: nil, keyEquivalent: ""))
+        for pr in prs {
+            let menuItem = NSMenuItem(title: "\(pr.title)", action: #selector(open), keyEquivalent: "")
+            menuItem.representedObject = pr as AnyObject
+            menu.addItem(menuItem)
+        }
+    }
+   
+    func buildTitle(list: [PR], emoji: String) -> String{
+        if !list.isEmpty {
+            return "  \(emoji) \(list.count)"
+        }
+        return ""
+    }
+    
+    @objc
+    func open(sender: NSMenuItem) {
+        let pr = sender.representedObject as! PR
+        NSWorkspace.shared.open(URL(string: pr.url)!)
+    }
+    
+    @objc
+    func quit() {
+        NSApplication.shared.terminate(self)
+    }
+
+    
+    @objc
+    func preferences() {
+        self.preferencesWindow!.makeKeyAndOrderFront(nil)
+        NSApp.activate(ignoringOtherApps: true)
+    }
+    
+    func updateMenu(result: Result?) {
+        if menuOpen {
+            return
+        }
+
+        self.menu.removeAllItems()
+
+        var title = ""
+        if let result = result {
+            for (k, v) in result.data.sorted(by: {$0.key < $1.key } ) {
+                let display = result.metadata.display[k, default: Display(symbol: "", title: k)]
+                title += self.buildTitle(list: v, emoji: display.symbol)
+                self.createMenuItems(v, title: display.title)
+            }
+            
+            title = title.trimmingCharacters(in: .whitespacesAndNewlines)
+            if title == "" {
+                title = "✓"
+            }
+        }
+        else {
+            title = "!"
+        }
+        self.statusBarItem.button?.title = title
+        self.menu.addItem(NSMenuItem.separator())
+        self.menu.addItem(NSMenuItem.init(title: "Preferences", action: #selector(self.preferences), keyEquivalent: ""))
+        self.menu.addItem(NSMenuItem.init(title: "Refresh", action: #selector(self.update), keyEquivalent: ""))
+        self.menu.addItem(NSMenuItem.init(title: "Quit", action: #selector(self.quit), keyEquivalent: ""))
+    }
+    
+    @objc
+    func update() {
+        if menuOpen {
+            return
+        }
+        
+        DispatchQueue.global(qos: .background).async {
+            if var base_url = UserDefaults.standard.string(forKey: "url") {
+                // You need to create the file url.swift that looks like: let base_url = "https://some.server/"
+                if !base_url.hasSuffix("/") {
+                    base_url += "/"
+                }
+                let url = URL(string: base_url + "\(NSUserName())")
+                
+                do {
+                    let webString = try String(contentsOf: url!)
+                    guard let json_data : Data = webString.data(using: .utf8) else { return }
+                    
+                    let result = try JSONDecoder().decode(Result.self, from: json_data)
+                    
+                    DispatchQueue.main.async {
+                        self.updateMenu(result: result)
+                    }
+                    
+                }
+                catch {
+                }
+            }
+            else {
+                DispatchQueue.main.async {
+                    self.preferences()
+                    self.updateMenu(result: nil)
+                }
+            }
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,83 @@
+# DevBar
+
+![](screenshot.png)
+
+DevBar is a workflow tool for developers. It watches a URL and displays a menu bar so you can easily see actions you can take, like pull requests that needs a review, PRs that have failing tests, etc.
+
+
+# Usage
+
+Start DevBar. It will ask you for a URL to watch, and it will append your username to that url (so if you give it https://example.com/foo it will access https://example.com/foo/your-username). The endpoint should reply with a JSON document that looks something like this:
+
+
+```json
+{
+    "data": {
+        "needs_work": [
+            {
+                "title": "Trying to fix the gadget widget",
+                "url": "https://github.com/example/prs/1",
+            }
+        ],
+        "other_problems": [],
+        "waiting_for_review": [
+            {
+                "title": "Stop the crashing",
+                "url": "https://github.com/example/prs/2",
+            },
+            {
+                "title": "Cool new feature",
+                "url": "github.com/example/prs/3",
+            }
+        ],
+        "devtest": [],
+        "ready_to_merge": [],
+        "workflow_problem": [],
+        "wip": []
+    },
+    "metadata": {
+        "display": {
+            "prod_crash": {
+                "priority": 0,
+                "symbol": "💥",
+                "title": "💥 Prod crash"
+            },
+            "other_problems": {
+                "priority": 10,
+                "symbol": "😟",
+                "title": "😟 Other problems"
+            },
+            "waiting_for_review": {
+                "priority": 10,
+                "symbol": "🕐",
+                "title": "🕐 Waiting for review"
+            },
+            "devtest": {
+                "priority": 10,
+                "symbol": "🧪",
+                "title": "🧪 Can be tested by developers"
+            },
+            "ready_to_merge": {
+                "priority": 10,
+                "symbol": "🎉",
+                "title": "🎉 Ready to merge"
+            },
+            "workflow_problem": {
+                "priority": 10,
+                "symbol": "🤨",
+                "title": "🤨 Workflow problem: should be 4EYE or ready for test"
+            },
+            "wip": {
+                "priority": 11,
+                "symbol": "🚧",
+                "title": "🚧 Work in progress"
+            }
+        }
+    }
+}
+```
+
+`metadata.display` is a dict of the different workflow types you've got. The symbol is used in the menu bar as a compact representation and the title is the full title displayed in the drop down menu.
+
+
+You can give it a spin by pointing it to the example.json file: `https://raw.githubusercontent.com/boxed/DevBar/master/example.json?`
